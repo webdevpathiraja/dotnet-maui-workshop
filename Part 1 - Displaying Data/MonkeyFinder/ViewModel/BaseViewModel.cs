ced5f99176e0710808f4538c3b56cb0eb714bdb0
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2025-07-17 23:57:53 +0530

    updated BaseViewModel.cs

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,5 +1,14 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
-public class BaseViewModel
+namespace MonkeyFinder.ViewModel;
+public partial class BaseViewModel : ObservableObject
 {
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+    bool isBusy;
+
+    [ObservableProperty]
+    string title;
+
+    public bool IsNotBusy => !IsBusy;
 }
