d095ae94ccfea4908c9a51947d006b9ebdef7204
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2025-07-18 09:19:59 +0530

    Register MainPage, MonkeysViewModel, and MonkeyService in Dependency Injection container
    - Added dependency injection setup for MonkeyService and MonkeysViewModel
    - Registered MainPage as the application's main UI page
    - Ensured proper namespace usage with using directives
    - Enabled debug logging in DEBUG builds

--- Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
+++ Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
@@ -1,26 +1,31 @@
 ﻿using Microsoft.Extensions.Logging;
+using MonkeyFinder.Services;
 using MonkeyFinder.View;
 
 namespace MonkeyFinder;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        // Register services and pages
+        builder.Services.AddSingleton<MonkeyService>();
+        builder.Services.AddSingleton<MonkeysViewModel>();
+        builder.Services.AddSingleton<MainPage>();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		
-
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
